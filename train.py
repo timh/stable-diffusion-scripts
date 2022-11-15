829fb64b2e9bb098f5c7b3bc7b95bc17c4fb2ef8
Tim Hinderliter <tim@disorder.net>
2022-11-14 23:23:15 -0800

    miror cleanups to train script

--- train.py
+++ train.py
@@ -4,39 +4,6 @@ import argparse
 import subprocess
 from typing import List
 
-# for seed in 666 777
-# do (
-# export OUTPUT_DIR="$TOP/outputs/alexhin24-inpainting5_${seed}"
-# accelerate launch train_inpainting_dreambooth.py \
-#   --output_dir=$OUTPUT_DIR \
-#   --class_data_dir=$CLASS_DIR \
-#   --instance_data_dir=$INSTANCE_DIR \
-#   --instance_prompt="photo of alexhin" \
-#   --class_prompt="photo of a white woman" \
-#   --num_class_images=76 \
-#   --learning_rate=2e-6 \
-#   --save_interval=1000 \
-#   --save_infer_steps=35 \
-# \
-#   --seed=$seed \
-#   --pretrained_model_name_or_path=$MODEL_NAME  \
-#   --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
-#   --with_prior_preservation --prior_loss_weight=1.0 \
-#   --resolution=512 \
-#   --train_batch_size=1 \
-#   --train_text_encoder \
-#   --sample_batch_size=1 \
-#   --gradient_accumulation_steps=1 --gradient_checkpointing \
-#   --use_8bit_adam \
-#   --lr_scheduler="polynomial" \
-#   --lr_warmup_steps=0 \
-#   --max_train_steps=$STEPS \
-#   --mixed_precision=fp16 \
-#   --not_cache_latents \
-#   --hflip
-# )
-# done
-
 class Config(argparse.Namespace):
     input_model_name: str
 
@@ -46,16 +13,15 @@ class Config(argparse.Namespace):
         if not os.path.isdir(self.instance_dir):
             raise Exception(f"instance_dir {self.instance_dir} doesn't exist")
 
-        self.output_dir = f"{self.output_root}/{self.name}"
         if self.inpainting:
             self.input_model_name = "runwayml/stable-diffusion-inpainting"
         else:
             self.input_model_name = "runwayml/stable-diffusion-v1-5"
         
-    def _output_dir(self, seed:int):
+    def _output_dir(self, seed:int) -> str:
         return f"{self.output_root}/{self.name}_r{seed}"
 
-    def _find_prior_steps_trained(self, output_dir) -> int:
+    def _find_prior_steps_trained(self, output_dir:str) -> int:
         if not os.path.exists(output_dir):
             return 0
         
@@ -67,25 +33,23 @@ class Config(argparse.Namespace):
         
         return max_steps
     
-    def run_one(self, seed):
-        input_model_name = self.input_model_name
+    def run_one(self, seed:int):
+        input_model_name:str = self.input_model_name
 
         output_dir_base = self._output_dir(seed)
         prior_steps = self._find_prior_steps_trained(output_dir_base)
+        max_train_steps:int = self.max_train_steps - prior_steps
+        output_dir = output_dir_base
         if prior_steps != 0:
             print(f"{output_dir_base}: found existing model with {prior_steps} steps already trained")
             input_model_name = f"{output_dir_base}/{prior_steps}"
-            max_train_steps = self.max_train_steps - prior_steps
             output_dir = f"{output_dir_base}+{prior_steps}"
-        else:
-            output_dir = output_dir_base
-            max_train_steps = self.max_train_steps
         
         if max_train_steps <= 0:
             print(f"** nothing to do, max_train_steps is {max_train_steps}")
             return
 
-        train_py = "train_inpainting_dreambooth.py" if self.inpainting else "train_dreambooth.py"
+        train_py:str = "train_inpainting_dreambooth.py" if self.inpainting else "train_dreambooth.py"
         args = ["accelerate", "launch", train_py,
                 "--output_dir", output_dir,
                 "--instance_data_dir", self.instance_dir,
@@ -116,17 +80,6 @@ class Config(argparse.Namespace):
                          "--not_cache_latents",
                          "--hflip"])
 
-        # write the config we used for this
-        if not self.dry_run:    
-            for steps in range(self.save_interval, max_train_steps+1, self.save_interval):
-                dirname = f"{output_dir}/{steps}"
-                filename = f"{dirname}/train-cmdline.txt"
-                os.makedirs(dirname, exist_ok=True)
-                with open(filename, "w") as output:
-                    output.write(f"{' '.join(args)}\n")
-                    output.write(f"# --max_train_steps: {self.max_train_steps}\n")
-                    output.write(f"# --output_root: {self.output_root}\n")
-        
         print(f"run_one:")
         print(f"       output_dir: {output_dir}")
         print(f"  max_train_steps: {self.max_train_steps}")
@@ -140,6 +93,16 @@ class Config(argparse.Namespace):
             res = subprocess.run(args, stdout=sys.stdout, stderr=sys.stderr, check=True)
             print(res.stdout)
 
+            # write the config we used for this
+            for steps in range(self.save_interval, max_train_steps+1, self.save_interval):
+                dirname = f"{output_dir}/{steps}"
+                filename = f"{dirname}/train-cmdline.txt"
+                os.makedirs(dirname, exist_ok=True)
+                with open(filename, "w") as output:
+                    output.write(f"{' '.join(args)}\n")
+                    output.write(f"# --max_train_steps: {self.max_train_steps}\n")
+                    output.write(f"# --output_root: {self.output_root}\n")
+        
         if prior_steps != 0:
             for steps in range(self.save_interval, max_train_steps+1, self.save_interval):
                 src = f"{output_dir}/{steps}"
