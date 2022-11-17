6666aaa5049239188a8ac1d21ddb58d1e296d80f
Tim Hinderliter <tim@disorder.net>
2022-11-17 09:25:04 -0800

    change num/threads per process to 4 so it works well locally, too

--- gen-many.py
+++ gen-many.py
@@ -49,7 +49,7 @@ def gen(config: argparse.Namespace):
             close_proc(last_cmd, proc)
             cmd = [
                 "accelerate", "launch",
-                "--num_cpu_threads_per_process", "8",
+                "--num_cpu_threads_per_process", "4",
                 "scripts/invoke.py",
                 "--model", one.model,
                 "--from_file", "-"
