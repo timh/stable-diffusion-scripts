52cd52d238df815c101b5dd57cd9b28517196cec
Tim Hinderliter <tim@disorder.net>
2022-11-19 19:55:00 -0800

    get rid of futzing with "archive" link, cuz it's fixed by upgrading pythong lib transformers(?? i think)

--- links.sh
+++ links.sh
@@ -14,7 +14,6 @@ do(
   else
     MODEL_PATH=$OUTPUT_DIR/$steps
     CHECKPOINT_PATH=${OUTPUT_DIR}_`printf %05d $steps`.ckpt
-    CHECKPOINT_PATH_ARCHIVE=${OUTPUT_DIR}/archive.ckpt
     if [ -f "$CHECKPOINT_PATH" ]
     then
       echo "$CHECKPOINT_PATH already exists, skipping..."
@@ -22,14 +21,11 @@ do(
       # create the CKPT, which is actually a zip file. some consumers of the CKPT file expect 
       # the top directory to be named "archive", so fake it with symlinks.
       echo "creating $CHECKPOINT_PATH..."
-      ln -s "$MODEL_PATH" archive
-      python ../../scripts/convert_diffusers_to_original_stable_diffusion.py --model_path archive --checkpoint_path $CHECKPOINT_PATH_ARCHIVE
-      mv $CHECKPOINT_PATH_ARCHIVE $CHECKPOINT_PATH
+      python $TOP/diffusers/scripts/convert_diffusers_to_original_stable_diffusion.py --model_path $MODEL_PATH --checkpoint_path $CHECKPOINT_PATH
       echo "$CHECKPOINT_PATH created"
 
       # add the text files with training parameters to the checkpoint
       zip -u $CHECKPOINT_PATH archive/*.txt
-      rm -f archive
     fi
     ln -sf "$CHECKPOINT_PATH" "$TOP/stable-diffusion-webui/models/Stable-diffusion/"
   fi
