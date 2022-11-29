57359a72bbd60742ead359b08835e1e693f0ff7e
Tim Hinderliter <tim@disorder.net>
2022-11-28 20:25:58 -0800

    making progress on typescript imagegrid

--- imagegrid2.ts
+++ imagegrid2.ts
@@ -1,24 +1,40 @@
-interface ImageSet {
-    model_name: string
-    model_seed: string
-    model_steps: number
-    prompt: string
-    sampler: string
-    sampler_steps: number
-    cfg: number
-
-    key: string
-}
-
-interface Image {
-    picset: ImageSet
+class GImage {
     filename: string
     seed: number
 }
 
-var models = Array<Model>()
-var imageSets = Array<ImageSet>()
-var images = Array<Image>()
+var sortKeys = ['modelName', 'modelSeed', 'modelSteps', 'prompt', 'sampler', 'samplerSteps', 'cfg']
+class GImageSet {
+    modelName: string
+    modelSeed: number = 0
+    modelSteps: number = 0
+    prompt: string
+    sampler: string
+    samplerSteps: number = 0
+    cfg: number = 0
+
+    images: Array<GImage> = []
+
+    getKey(): string {
+        var res = ""
+        sortKeys.forEach((key) => {
+            if (res) {
+                res += ", "
+            }
+            var val = this[key]
+            if (typeof val == "number") {
+                val = val.toString().padStart(5, "0")
+            }
+            else {
+                val = val.toString()
+            }
+            res += (key + "=" + val)
+        })
+        return res
+    }
+}
+
+var imageSets = new Map<string, GImageSet>()
 
 const RE_FILENAME = /(.+[\d_]+)--(.+)--([\w\+\d_,]+)\/\d+\.(\d+)\.png/
 const RE_SAMPLER = /([\w\+_]+)_(\d+),c(\d+)/
@@ -29,28 +45,61 @@ async function updateList() {
     
     const data = await resp.text()
     if (resp.ok) {
-        var lines = data.split("\n")
-        lines.forEach((line) => {
-            var match = RE_FILENAME.exec(line);
+        var filenames = data.split("\n")
+        filenames.forEach((filename) => {
+            var match = RE_FILENAME.exec(filename)
             if (match) {
-                var model_str = match[1]
-                var prompt = match[2]
+                var iset = new GImageSet()
+                var img = new GImage()
+                img.filename = filename
+
+                var modelStr = match[1]
+                iset.prompt = match[2]
                 var samplerStr = match[3]
                 var seedStr = match[4]
-                var seed = parseInt(seedStr)
-                var sampler = samplerStr
-                var samplerSteps = 0
-                var cfg = 0
+                img.seed = parseInt(seedStr)
 
                 match = RE_SAMPLER.exec(samplerStr)
                 if (match) {
-                    sampler = match[1]
-                    samplerSteps = parseInt(match[2])
-                    cfg = parseInt(match[3])
+                    iset.sampler = match[1]
+                    iset.samplerSteps = parseInt(match[2])
+                    iset.cfg = parseInt(match[3])
                 }
-                console.log(`model_str ${model_str}, prompt '${prompt}', sampler ${sampler} ${samplerSteps}, cfg ${cfg}, seed ${seed}`)
+
+                var modelSeed = 0
+                var modelSteps = 0
+                match = RE_MODEL.exec(modelStr)
+                if (match) {
+                    iset.modelName = match[1]
+                    iset.modelSeed = parseInt(match[2])
+                    iset.modelSteps = parseInt(match[3])
+                }
+                else {
+                    iset.modelName = modelStr
+                }
+
+                var isetKey = iset.getKey()
+                if (imageSets.has(isetKey)) {
+                    iset = imageSets.get(isetKey) as GImageSet
+                }
+                else {
+                    imageSets.set(isetKey, iset)
+                }
+
+                // add an image to the imageset.
+                iset.images.push(img)
             }
         })
+
+        var sortedKeys = Array.from(imageSets.keys()).sort()
+        // imageSets.forEach((val, key) => {
+        //     console.log(`${key} has ${val.images.length}`)
+        // });
+        sortedKeys.forEach((key) => {
+            var val = imageSets.get(key)
+            console.log(`${key} has ${val!.images.length}`)
+    })
+        //console.log(imageSets.keys())
     }
     else {
         console.log(`error`)
