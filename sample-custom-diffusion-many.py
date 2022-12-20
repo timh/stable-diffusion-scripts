1588d9ba0a60cfd6ac91da38afed8004f336ec02
Tim Hinderliter <tim@disorder.net>
2022-12-20 15:39:04 -0800

    squash them together: WIP training & sampling for custom diffusion

--- sample-custom-diffusion-many.py
+++ sample-custom-diffusion-many.py
@@ -1,10 +1,10 @@
 import argparse
 import sys
-import os
+import os, os.path
 import numpy as np
 sys.path.append('./')
 import torch
-from diffusers import StableDiffusionPipeline, AutoencoderKL
+from diffusers import StableDiffusionPipeline, AutoencoderKL, DPMSolverMultistepScheduler
 from typing import List
 from PIL import Image, ImageFont, ImageDraw
 from pathlib import Path
@@ -14,69 +14,75 @@ import math
 from fonts.ttf import Roboto
 
 DEFAULT_SEED = 1
-INF_STEPS = 50
+INF_STEPS = 30
 CFG = 6.0
-FONT_SIZE = 30
-FONT_SIZE_BIG = 50
-def sample(ckpt, delta_ckpts, prompt, token, freeze_model, seed):
-    model_id = ckpt
+FONT_SIZE = 24
+FONT_SIZE_BIG = 40
 
-    big_image: Image.Image = None
-
+def sample(model_id: str, delta_ckpts: List[str], prompt: str, token: str, freeze_model: str, seed: int, output: str):
     num_width = min(4, len(delta_ckpts))
     num_height = math.ceil(len(delta_ckpts) / num_width)
 
     font_small = ImageFont.truetype(Roboto, FONT_SIZE)
     font_big = ImageFont.truetype(Roboto, FONT_SIZE_BIG)
+    out_image: Image.Image = None
 
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None).to("cuda")
-    # pipe.vae = AutoencoderKL.from_pretrained("/home/tim/models/sd-vae-ft-mse", torch_dtype=torch.float16).to("cuda")
-    # print(f"pipe.vae = {pipe.vae.__class__.__name__}")
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config, algorithm_type='dpmsolver++', solver_order=1)
+
+    common_root = os.path.commonprefix([Path(d).parent for d in delta_ckpts])
+
     for delta_idx, delta_ckpt in enumerate(delta_ckpts):
         print(f"{delta_ckpt}:")
         diffuser_training.load_model(pipe.text_encoder, pipe.tokenizer, pipe.unet, delta_ckpt, token, freeze_model)
 
         generator = torch.Generator("cuda").manual_seed(seed)
         image: Image.Image = pipe(prompt, generator=generator, num_inference_steps=INF_STEPS, guidance_scale=CFG, eta=1., num_images_per_prompt=1).images[0]
-        if big_image is None:
+        if out_image is None:
             width = image.width * num_width
             height = image.height * num_height
-            big_image = Image.new(mode="RGB", size=(width, height))
-            draw = ImageDraw.Draw(big_image)
+            out_image = Image.new(mode="RGB", size=(width, height))
+            draw = ImageDraw.Draw(out_image)
 
         x = (delta_idx % num_width) * image.width
         y = int(delta_idx / num_width) * image.height
-        big_image.paste(image, (x, y))
+        out_image.paste(image, (x, y))
 
-        filename_short = delta_ckpt.replace("/home/tim/models/", "").replace("/", "/\n ")
-        text = filename_short
+        text = delta_ckpt.replace("/home/tim/models/", "").replace("/", "/\n ")
         textx = x + 2
         texty = y + 10
         draw.text(xy=(textx+1, texty+1), text=text, font=font_small, fill="black")
         draw.text(xy=(textx, texty), text=text, font=font_small, fill="white")
 
-    text = f"'{prompt}'\ncfg: {CFG}, inference steps: {INF_STEPS}, seed: {seed}"
-    textx = 2
-    texty = height - FONT_SIZE_BIG * len(text.split("\n")) - 2
-    draw.text(xy=(textx + 1, texty + 1), text=text, font=font_big, fill="black")
-    draw.text(xy=(textx, texty), text=text, font=font_big, fill="white")
+    text = f"'{prompt}'\n"
+    text += f"cfg: {CFG}, inference steps: {INF_STEPS}, seed: {seed}\n"
+    text += f"base: {Path(model_id).name}"
 
-    big_image.save("output-temp.png")
-    os.system("mv output-temp.png output.png")
+    textx = width - 2
+    texty = height - 2
+    draw.text(xy=(textx + 1, texty + 1), text=text, font=font_big, fill="black", anchor="rd")
+    draw.text(xy=(textx, texty), text=text, font=font_big, fill="white", anchor="rd")
+
+    output_temp = f"{output}-temp.png"
+    out_image.save(output_temp)
+    os.system(f"mv {output_temp} {output}")
+
+    print(f"created {output}")
 
 
 
 def parse_args():
-    parser = argparse.ArgumentParser('', add_help=False)
-    parser.add_argument('--ckpt', help='base model', type=str)
-    parser.add_argument('--token', help='token, e.g. <new1>', type=str, default="<new1>")
-    parser.add_argument('--delta_ckpt', dest='delta_ckpts', help='delta.bin', default=None, nargs='+')
-    parser.add_argument('--prompt', help='prompt to generate', default=None, type=str)
-    parser.add_argument('--freeze_model', help='crossattn or crossattn_kv', default='crossattn_kv', type=str)
-    parser.add_argument('--seed', help='random seed', default=DEFAULT_SEED, type=int)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", dest="model_id", help="base diffusers model name or path", default="runwayml/stable-diffusion-v1-5", type=str)
+    parser.add_argument("--token", help="token, e.g. <new1>", type=str)
+    parser.add_argument("--delta", dest="delta_ckpts", help="filename(s) to delta.bin", default=None, nargs='+')
+    parser.add_argument("--prompt", help="prompt to generate", default=None, type=str)
+    parser.add_argument("--freeze_model", help="crossattn or crossattn_kv", default="crossattn_kv", type=str)
+    parser.add_argument("--seed", help="random seed", default=DEFAULT_SEED, type=int)
+    parser.add_argument("-o", "--out", "--output", dest="output", help="filename of output PNG", default="output.png", type=str)
 
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
-    sample(args.ckpt, args.delta_ckpts, args.prompt, args.token, args.freeze_model, args.seed)
+    sample(model_id=args.model_id, delta_ckpts=args.delta_ckpts, prompt=args.prompt, token=args.token, freeze_model=args.freeze_model, seed=args.seed, output=args.output)
--- train-custom-diffusion.py
+++ train-custom-diffusion.py
@@ -5,28 +5,30 @@ import subprocess
 MODEL_NAME = "runwayml/stable-diffusion-v1-5"
 # MODEL_NAME = "/home/tim/models/stable-diffusion-v1-5+vae"
 
+# BATCH = 2
 BATCH = 1
+MODIFIER_TOKEN = None
+TRAIN_TEXT_ENCODER = True
+FREEZE_CROSSATTN = True
 
-# defaults from their example
-if False:
-    LR = "1e-5"
-    STEPS = 250
-    MODIFIER_TOKEN = "<new1>"
-    INSTANCE_PROMPT = "photo of a <new1> cat"
-    INSTANCE_DIR = "./data/cat"
-    CLASS_DIR = "./real_reg/samples_cat/"
-    CLASS_PROMPT = "cat"
-    OUTPUT_DIR = "./logs/cat"
-else:
-    # for human faces 5e-6
-    LR = "5.0e-6"
-    MODIFIER_TOKEN = "timhin"
-    INSTANCE_PROMPT = "timhin person"
-    INSTANCE_DIR = "/home/tim/devel/images.tim/15-512px"
-    CLASS_DIR = "/home/tim/devel/class_images-MirageML/Mix"
-    CLASS_PROMPT = "person"
-    STEPS = len(list(Path(INSTANCE_DIR).iterdir())) * 500
-    OUTPUT_DIR = f"/home/tim/models/tim15-custom/timperson15@{LR}_{STEPS:05}"
+LR = "5.0e-6"
+INSTANCE_PROMPT = "timhin person"
+INSTANCE_DIR = "/home/tim/devel/images.tim/15-512px"
+CLASS_DIR = "/home/tim/devel/class_images-MirageML/Mix"
+CLASS_PROMPT = "person"
+NUM_INSTANCES = len(list(Path(INSTANCE_DIR).iterdir()))
+STEPS = NUM_INSTANCES * 500
+SAVE_STEPS = int(STEPS / 10)
+
+extra = ""
+if TRAIN_TEXT_ENCODER:
+    extra = extra + "+text"
+if BATCH > 1:
+    extra = extra + f"+batch{BATCH}"
+if FREEZE_CROSSATTN:
+    extra = extra + "+crossattn"
+
+OUTPUT_DIR = f"/home/tim/models/tim15-custom/timperson15{extra}@{LR}"
 
 args = [
     "accelerate", "launch", "src/diffuser_training.py",
@@ -36,7 +38,9 @@ args = [
 
     "--instance_data_dir", INSTANCE_DIR,
     "--instance_prompt", INSTANCE_PROMPT,
-    "--modifier_token", MODIFIER_TOKEN,
+    *(["--modifier_token", MODIFIER_TOKEN] if MODIFIER_TOKEN else []),
+    *(["--train_text_encoder"] if TRAIN_TEXT_ENCODER else []),
+    *(["--freeze_model", "crossattn"] if FREEZE_CROSSATTN else []),
 
     "--class_data_dir", CLASS_DIR,
     "--class_prompt", CLASS_PROMPT,
@@ -48,9 +52,11 @@ args = [
     "--learning_rate", LR,
     "--lr_warmup_steps=0",
     "--max_train_steps", str(STEPS),
+    "--save_steps", str(SAVE_STEPS),
     "--scale_lr",
 
-    "--mixed_precision", "no"
+    "--mixed_precision", "no", # must be set to 'no'. if 'bf16' or 'fp16', it crashes due to unimplemented function
+    *(["--gradient_checkpointing", "--use_8bit_adam"] if BATCH > 1 else []),
 ]
 
 cmdline = shlex.join(args)
