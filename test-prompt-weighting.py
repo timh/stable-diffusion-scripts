d4092a1e33d6fe43e68d21d4b0e01f18d7a5045e
Tim Hinderliter <tim@disorder.net>
2023-01-11 23:43:03 -0800

    WIP prompt weighting: getting closer, but still no cigar

--- test-prompt-weighting.py
+++ test-prompt-weighting.py
@@ -18,137 +18,159 @@ MODEL = "/home/tim/models/alex66-kohyanative-modelshoot-batch11@0.5_r2/epoch-000
 
 SIZE = 512
 # PROMPT = "portrait of alexhin, pencil sketch"
-PROMPT = "portrait of alexhin, dramatic lighting, tone mapped, elegant, digital painting, artstation, smooth, sharp focus"
-BASE_SEED = 0
+PROMPT = "portrait photo of alexhin wearing a blue hat"
+BASE_SEED = 1
 
-@torch.no_grad()
-def gen(
-    pipe: StableDiffusionPipeline,
-    prompt: Union[str, List[str]],
-    height: Optional[int] = None,
-    width: Optional[int] = None,
-    num_inference_steps: int = 50,
-    guidance_scale: float = 7.5,
-    negative_prompt: Optional[Union[str, List[str]]] = None,
-    num_images_per_prompt: Optional[int] = 1,
-    eta: float = 0.0,
-    generator: Optional[Union[torch.Generator, List[torch.Generator]]] = None,
-    latents: Optional[torch.FloatTensor] = None,
-    callback: Optional[Callable[[int, int, torch.FloatTensor], None]] = None,
-    callback_steps: Optional[int] = 1,
-):
-        # 0. Default height and width to unet
-        height = height or pipe.unet.config.sample_size * pipe.vae_scale_factor
-        width = width or pipe.unet.config.sample_size * pipe.vae_scale_factor
+def encode_part(self: StableDiffusionPipeline, prompt: str, device: torch.device):
+    text_inputs = self.tokenizer(
+        prompt,
+        #padding="max_length",
+        #max_length=self.tokenizer.model_max_length,
+        #truncation=True,
+        return_tensors="pt",
+    )
+    text_input_ids = text_inputs.input_ids
+    untruncated_ids = self.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
 
-        # 1. Check inputs. Raise error if not correct
-        pipe.check_inputs(prompt, height, width, callback_steps)
+    if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(text_input_ids, untruncated_ids):
+        removed_text = self.tokenizer.batch_decode(untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1])
+        print(
+            "The following part of your input was truncated because CLIP can only handle sequences up to"
+            f" {self.tokenizer.model_max_length} tokens: {removed_text}"
+        )
 
-        # 2. Define call parameters
-        batch_size = 1 if isinstance(prompt, str) else len(prompt)
-        device = pipe._execution_device
-        # here `guidance_scale` is defined analog to the guidance weight `w` of equation (2)
-        # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
-        # corresponds to doing no classifier free guidance.
-        do_classifier_free_guidance = guidance_scale > 1.0
+    if hasattr(self.text_encoder.config, "use_attention_mask") and self.text_encoder.config.use_attention_mask:
+        attention_mask = text_inputs.attention_mask.to(device)
+    else:
+        attention_mask = None
 
-        # START TIM
-        text_inputs = pipe.tokenizer(
-            prompt,
+    text_embeddings = self.text_encoder(
+        text_input_ids.to(device),
+        attention_mask=attention_mask,
+    )
+    text_embeddings = text_embeddings[0]
+
+    print(f"prompt '{prompt}'")
+    print(f"  text_input_ids size {text_input_ids.size()}")
+    print(f"  text_input_ids {text_input_ids}")
+    print(f"  part_embeddings size {text_embeddings.size()}")
+
+    return text_embeddings
+
+def _encode_prompt_fun(self: StableDiffusionPipeline):
+    def fun(*args, **kwargs):
+        return _encode_prompt(self, *args, **kwargs)
+    return fun
+
+def _encode_prompt(self: StableDiffusionPipeline, prompt: str, device: torch.device, num_images_per_prompt: int, do_classifier_free_guidance: bool, negative_prompt: str):
+    r"""
+    Encodes the prompt into text encoder hidden states.
+
+    Args:
+        prompt (`str` or `list(int)`):
+            prompt to be encoded
+        device: (`torch.device`):
+            torch device
+        num_images_per_prompt (`int`):
+            number of images that should be generated per prompt
+        do_classifier_free_guidance (`bool`):
+            whether to use classifier free guidance or not
+        negative_prompt (`str` or `List[str]`):
+            The prompt or prompts not to guide the image generation. Ignored when not using guidance (i.e., ignored
+            if `guidance_scale` is less than `1`).
+    """
+    batch_size = len(prompt) if isinstance(prompt, list) else 1
+
+    ##
+    ## TIM HACKS
+    ##
+    text_embeddings: torch.Tensor = None
+    out_idx = 1
+    for part_idx, part in enumerate(prompt.split(",")):
+        part = part.strip()
+        if part_idx > 0:
+            part = ", " + part
+        part_embeddings = encode_part(self, part, device)
+        if text_embeddings is None:
+            text_embeddings = torch.zeros(torch.Size([1, self.tokenizer.model_max_length, part_embeddings.shape[-1]]), dtype=part_embeddings.dtype).to(device)
+        num_embeddings = part_embeddings.shape[1]
+
+        # copy the embeddings, excluding start and end.
+        for idx in range(num_embeddings - 2):
+            text_embeddings[0][out_idx + idx] = part_embeddings[0][idx + 1]
+        out_idx += num_embeddings
+
+    # now add start & end embeddings.    
+    special_input_ids = self.tokenizer("", return_tensors="pt").input_ids
+    print(f"special_input_ids = {special_input_ids}")
+    special_embedding = self.text_encoder(special_input_ids.to(device))[0]
+    text_embeddings[0][0] = special_embedding[0][0]
+    # for idx in range(out_idx, self.tokenizer.model_max_length):
+    #     text_embeddings[0][idx] = special_embedding[0][1]
+    text_embeddings[0][out_idx] = special_embedding[0][1]
+    ##
+    ## END TIM HACKS
+    ##
+
+    # duplicate text embeddings for each generation per prompt, using mps friendly method
+    bs_embed, seq_len, _ = text_embeddings.shape
+    text_embeddings = text_embeddings.repeat(1, num_images_per_prompt, 1)
+    text_embeddings = text_embeddings.view(bs_embed * num_images_per_prompt, seq_len, -1)
+
+    # get unconditional embeddings for classifier free guidance
+    if do_classifier_free_guidance:
+        uncond_tokens: List[str]
+        if negative_prompt is None:
+            uncond_tokens = [""] * batch_size
+        elif type(prompt) is not type(negative_prompt):
+            raise TypeError(
+                f"`negative_prompt` should be the same type to `prompt`, but got {type(negative_prompt)} !="
+                f" {type(prompt)}."
+            )
+        elif isinstance(negative_prompt, str):
+            uncond_tokens = [negative_prompt]
+        elif batch_size != len(negative_prompt):
+            raise ValueError(
+                f"`negative_prompt`: {negative_prompt} has batch size {len(negative_prompt)}, but `prompt`:"
+                f" {prompt} has batch size {batch_size}. Please make sure that passed `negative_prompt` matches"
+                " the batch size of `prompt`."
+            )
+        else:
+            uncond_tokens = negative_prompt
+
+        uncond_input = self.tokenizer(
+            uncond_tokens,
             padding="max_length",
-            max_length=pipe.tokenizer.model_max_length,
+            max_length=text_embeddings.shape[-2],
             truncation=True,
             return_tensors="pt",
         )
-        text_input_ids = text_inputs.input_ids
-        untruncated_ids = pipe.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
+        print(f"uncond_input.size() {uncond_input.input_ids.size()}")
 
-        if hasattr(pipe.text_encoder.config, "use_attention_mask") and pipe.text_encoder.config.use_attention_mask:
-            attention_mask = text_inputs.attention_mask.to(device)
+        if hasattr(self.text_encoder.config, "use_attention_mask") and self.text_encoder.config.use_attention_mask:
+            attention_mask = uncond_input.attention_mask.to(device)
         else:
             attention_mask = None
-        
-        text_embeddings = pipe.text_encoder(
-            text_input_ids.to(device),
+
+        uncond_embeddings = self.text_encoder(
+            uncond_input.input_ids.to(device),
             attention_mask=attention_mask,
         )
-        text_embeddings = text_embeddings[0]
+        uncond_embeddings = uncond_embeddings[0]
+        print(f"uncond_embeddings.size {uncond_embeddings.size()}")
 
-        manual_embeddings = torch.zeros(text_embeddings.size(), dtype=text_embeddings.dtype).to(device)
-        for idx, input_id in enumerate(text_input_ids):
-            idx_input_ids = torch.zeros(text_input_ids.size(), dtype=torch.int32).to(device)
-            idx_input_ids[idx] = input_id
-            input_embeddings = pipe.text_encoder(idx_input_ids)[0]
-            # if idx > 10:
-            #     input_embeddings = input_embeddings.mul(1.2)
-            manual_embeddings[idx] = input_embeddings[0] # * 1.2
+        # duplicate unconditional embeddings for each generation per prompt, using mps friendly method
+        seq_len = uncond_embeddings.shape[1]
+        uncond_embeddings = uncond_embeddings.repeat(1, num_images_per_prompt, 1)
+        uncond_embeddings = uncond_embeddings.view(batch_size * num_images_per_prompt, seq_len, -1)
 
-        builtin_embeddings = pipe._encode_prompt(prompt, device, num_images_per_prompt, do_classifier_free_guidance, negative_prompt)
-        uncond_builtin = builtin_embeddings[0:1]
-        text_embeddings = torch.cat([uncond_builtin, manual_embeddings])
+        # For classifier free guidance, we need to do two forward passes.
+        # Here we concatenate the unconditional and text embeddings into a single batch
+        # to avoid doing two forward passes
+        text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
 
-        bs_embed, seq_len, _ = text_embeddings.shape
-        text_embeddings = text_embeddings.repeat(1, num_images_per_prompt, 1)
-        text_embeddings = text_embeddings.view(bs_embed * num_images_per_prompt, seq_len, -1)
-
-        # END TIM
-        
-        # 4. Prepare timesteps
-        pipe.scheduler.set_timesteps(num_inference_steps, device=device)
-        timesteps = pipe.scheduler.timesteps
-
-        # 5. Prepare latent variables
-        num_channels_latents = pipe.unet.in_channels
-        latents = pipe.prepare_latents(
-            batch_size * num_images_per_prompt,
-            num_channels_latents,
-            height,
-            width,
-            text_embeddings.dtype,
-            device,
-            generator,
-            latents,
-        )
-
-        # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
-        extra_step_kwargs = pipe.prepare_extra_step_kwargs(generator, eta)
-
-        # 7. Denoising loop
-        num_warmup_steps = len(timesteps) - num_inference_steps * pipe.scheduler.order
-        with pipe.progress_bar(total=num_inference_steps) as progress_bar:
-            for i, t in enumerate(timesteps):
-                # expand the latents if we are doing classifier free guidance
-                latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
-                latent_model_input = pipe.scheduler.scale_model_input(latent_model_input, t)
-
-                # predict the noise residual
-                noise_pred = pipe.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
-
-                # perform guidance
-                if do_classifier_free_guidance:
-                    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
-
-                # compute the previous noisy sample x_t -> x_t-1
-                latents = pipe.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
-
-                # call the callback, if provided
-                if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % pipe.scheduler.order == 0):
-                    progress_bar.update()
-                    if callback is not None and i % callback_steps == 0:
-                        callback(i, t, latents)
-
-        # 8. Post-processing
-        image = pipe.decode_latents(latents)
-
-        # 9. Run safety checker
-        image, has_nsfw_concept = pipe.run_safety_checker(image, device, text_embeddings.dtype)
-
-        # 10. Convert to PIL
-        image = pipe.numpy_to_pil(image)
-
-        return StableDiffusionPipelineOutput(images=image, nsfw_content_detected=has_nsfw_concept)
-
+    print(f"text_embeddings.size {text_embeddings.size()}")
+    return text_embeddings
 
 def mem(s: str):
     free, used = torch.cuda.mem_get_info()
@@ -166,10 +188,11 @@ if __name__ == "__main__":
     font = ImageFont.truetype(Roboto, 30)
 
     generator = torch.Generator("cuda").manual_seed(BASE_SEED)
-    my_image = gen(pipe, PROMPT, generator=generator, num_inference_steps=30, guidance_scale=7).images[0]
+    standard_image = pipe(PROMPT, generator=generator, num_inference_steps=30, guidance_scale=7).images[0]
 
     generator = torch.Generator("cuda").manual_seed(BASE_SEED)
-    standard_image = pipe(PROMPT, generator=generator, num_inference_steps=30, guidance_scale=7).images[0]
+    pipe._encode_prompt = _encode_prompt_fun(pipe)
+    my_image = pipe(PROMPT, generator=generator, num_inference_steps=30, guidance_scale=7).images[0]
 
     overall_image.paste(standard_image, (0, 0))
     overall_image.paste(my_image, (SIZE, 0))
